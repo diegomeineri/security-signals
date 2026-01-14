de61934877476b222741d7533d087fb4197914b2
diegomeineri <39359016+diegomeineri@users.noreply.github.com>
2026-01-14 11:50:17 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "PrexSecuritySignals",
+    name: "SecuritySignals",
     platforms: [.iOS(.v14)],
     products: [
         .library(
-            name: "PrexSecuritySignals",
+            name: "SecuritySignals",
             targets: ["securitySignalsPlugin"])
     ],
     dependencies: [
